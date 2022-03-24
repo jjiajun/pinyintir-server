a2283fae3e50a45ae6361006201604296678e59d
jjiajun <teo.jia.jun.29@gmail.com>
2022-03-24 12:28:27 +0800

    ATTEMPT 2

--- controllers/phraseCtrl.js
+++ controllers/phraseCtrl.js
@@ -69,6 +69,7 @@ class PhraseController extends BaseController {
         },
         { new: true },
       );
+      console.log('FINAL CONSOLE LOG? ', result.phrases);
       res.send(result.phrases.filter((phrase) => phrase._id == phraseId)[0]);
     } catch (err) {
       this.errorHandler(err, res);
